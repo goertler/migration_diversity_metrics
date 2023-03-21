6be81d2af2e734d1b333e89cd6e1b3ee283c014a
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2023-03-21 15:56:48 -0700

    make stage data reproducible, with Ryan Peek's watershedtools

--- R/get_stage.R
+++ R/get_stage.R
@@ -1,48 +1,56 @@
 # library
 library(dplyr)
 library(imputeTS)
+library(devtools)
+
+devtools::install_github("ryanpeek/wateRshedTools")
+library(wateRshedTools)
 
 # stage at Rio Vista
-stage <- read.csv("data/enviro_data/RVB_Stage.csv")
-stage$Date <- as.Date(stage$Date)
-stage <- subset(stage, DATA_FLAG != "N")
+stage <- get_cdec("RVB", 1, "H", "2007-02-15", "2017-06-18")
+
 head(stage)
 str(stage)
-sum(is.na(stage$VALUE)) #1.2%
-plot(stage$Date, stage$VALUE) # a few outlier looking values in 2009
+stage$date <- as.Date(stage$datetime)
 
-#plot(stage_check$Date, stage_check$VALUE, data = subset(stage, Date < as.Date("2010-01-01") & Date > as.Date("2008-01-01")))
-stage_09 <- subset(stage, Date < as.Date("2009-11-01") & Date > as.Date("2009-09-01"))
-plot(stage_09$Date, stage_09$VALUE) # 1.19 looks like the correct break to remove those values
+sum(is.na(stage$value))
+plot(stage$date, stage$value) # two very large values
 
-stage_clean <- subset(stage, VALUE > 1.19)
+stage_qc <- subset(stage, value < 2000)
+
+plot(stage_qc$date, stage_qc$value) # a few outlier looking values in 2009
+
+stage_09 <- subset(stage_qc, date < as.Date("2009-11-01") & date > as.Date("2009-09-01"))
+plot(stage_09$date, stage_09$value) # 1.19 looks like the correct break to remove those values
+
+stage_clean <- subset(stage_qc, value > 1.19)
 
 # check sampling interval
 stage_freq <- stage_clean %>%
-  group_by(Date) %>%
+  group_by(date) %>%
   summarize(n()) # between 1 and 24
 
 #only include those with great than 20 measurements in a day (21/24 = 0.875)
-exclude_dates <- subset(stage_freq, `n()` < 20) # drops 46 days
+exclude_dates <- subset(stage_freq, `n()` < 20) # drops 38 days
 
-stage_cont = stage_clean[!(stage_clean$Date %in% exclude_dates$Date), ] #removes 624 values
+stage_cont = stage_clean[!(stage_clean$date %in% exclude_dates$date), ] #removes 509 values
 
 # make daily
 cv <- function(x) 100*( sd(x, na.rm = TRUE)/mean(x, na.rm = TRUE))
 
-stage_daily <- stage_cont[,c(7,10)] %>%
-  group_by(Date) %>%
+stage_daily <- stage_cont[,c(6,8)] %>%
+  group_by(date) %>%
   summarise_each(funs(mean = mean(., na.rm = TRUE), max = max(., na.rm = TRUE), min = min(., na.rm = TRUE), sd = sd(., na.rm = TRUE), cv, n = sum(!is.na(.))))
 
-stage_daily <- subset(stage_daily, n > 20) # still had a few, must have been NA values and real dates
+stage_daily <- subset(stage_daily, n > 20) # still had one, must have been NA values and real dates
 
 # check for missing days
-continous_dates <- data.frame (x = 1:3777, Date = seq(as.Date('2007-02-15'),as.Date('2017-06-18'),by='day'))
-stage_daily_cont <- merge(stage_daily, continous_dates, by = "Date", all = TRUE)
+continous_dates <- data.frame (x = 1:3777, date = seq(as.Date('2007-02-15'),as.Date('2017-06-18'),by='day'))
+stage_daily_cont <- merge(stage_daily, continous_dates, by = "date", all = TRUE)
 
 stage_daily_NA <- stage_daily_cont[is.na(stage_daily_cont$mean),] # only 51 out of 4000 obs.
 
-stage_daily_NA$group <- cumsum(c(1, diff.Date(stage_daily_NA$Date)) >= 2)
+stage_daily_NA$group <- cumsum(c(1, diff.Date(stage_daily_NA$date)) >= 2)
 
 stage_daily_NA_summary <- stage_daily_NA %>%
   group_by(group) %>%
@@ -51,5 +59,5 @@ stage_daily_NA_summary <- stage_daily_NA %>%
 
 # small enough, comfortable imputing missing data
 stage_daily_cont$mean <- na_ma(stage_daily_cont$mean, k = 7, weighting = "exponential", maxgap = Inf)
-colnames(stage_daily_cont)[1] <- "date"
-write.csv(stage_daily_cont[,-8], "results/SD/stage_daily_mean.csv")
+
+write.csv(stage_daily_cont[,c(1,2)], "data/stage_dat.csv")
