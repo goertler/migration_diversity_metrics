2ff8f21ce36b04b6cdc6116b6ddc4785ce744103
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2023-03-28 16:50:13 -0700

    sd data was cut too short, needed to add two weeks for CM fish

--- R/integrate_results_with_enviro.R
+++ R/integrate_results_with_enviro.R
@@ -11,30 +11,32 @@ flow <- read.csv("data/flow_dat.csv")
 head(model_dat)
 str(model_dat)
 
+# fix dates
 model_dat$rel <- as.Date(model_dat$rel)
 model_dat$end <- as.Date(model_dat$end)
+stage$date <- as.Date(stage$date)
+temperature$date <- as.Date(temperature$date)
+flow$date <- as.Date(flow$date)
 
 length(unique(model_dat$FishID))
 
-keeper_dat <- data.frame(FishID = NA, date_min = as.Date("1900-01-01"),  date_max = as.Date("1900-01-01"), temp_mean = NA, temp_sd = NA, stage_mean = NA, stage_sd = NA)
+keeper_dat <- data.frame(FishID = NA, date_min = as.Date("1900-01-01"),  date_max = as.Date("1900-01-01"), temp_mean = NA, temp_sd = NA, flow_mean = NA, flow_sd = NA, stage_sd = NA)
 
-for(i in unique(sd_meta$FishID)){
-  temp_dat <- subset(sd_meta, FishID == i)
-  temp_df <- subset(temp_daily, date >= min(temp_dat$rel) & date <= max(temp_dat$end))
-  stage_df <- subset(stage_daily_cont, date >= min(temp_dat$rel) & date <= max(temp_dat$end))
-  new_dat <- data.frame(FishID = i, date_min = min(temp_dat$rel),  date_max = max(temp_dat$end), temp_mean = mean(temp_df$mean), temp_sd = sd(temp_df$mean), stage_mean = mean(stage_df$mean), stage_sd = sd(stage_df$mean))
+for(i in unique(model_dat$FishID)){
+  temp_dat <- subset(model_dat, FishID == i)
+  temp_df <- subset(temperature, date >= min(temp_dat$rel) & date <= max(temp_dat$end))
+  stage_df <- subset(stage, date >= min(temp_dat$rel) & date <= max(temp_dat$end))
+  flow_df <- subset(flow, date >= min(temp_dat$rel) & date <= max(temp_dat$end))
+  new_dat <- data.frame(FishID = i, date_min = min(temp_dat$rel), date_max = max(temp_dat$end), temp_mean = mean(temp_df$mean), temp_sd = sd(temp_df$mean), flow_mean = mean(flow_df$sac), flow_sd = sd(flow_df$sac), stage_sd = sd(stage_df$mean))
   keeper_dat <- rbind(keeper_dat, new_dat)
 }
 
 head(keeper_dat)
 keeper_dat <- keeper_dat[-1,]
 
-model_df <- merge(keeper_dat[,-c(2:3)], sd_meta, by = "FishID", all = TRUE)
+model_df <- merge(keeper_dat[,-c(2:3)], model_dat, by = "FishID", all = TRUE)
 head(model_df)
 
 sapply(X = model_df, FUN = function(x) sum(is.na(x))) #just the 8 JSATS
-check_sd <- model_df[is.na(model_df$SD),]
 
-# missing fish size
-
-write.csv(model_df[,-6], "results/SD/model_dat.csv")
+write.csv(model_df, "results/model_dat.csv",  row.names=FALSE)
